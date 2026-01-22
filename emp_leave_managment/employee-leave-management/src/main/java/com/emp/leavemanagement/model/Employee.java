834f4b16058cc7e8e4913e258bac3283fad800eb
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-01-22 15:47:40 +0530

    Employee Model Created

---
+++ emp_leave_managment/employee-leave-management/src/main/java/com/emp/leavemanagement/model/Employee.java
@@ -0,0 +1,47 @@
+package com.emp.leavemanagement.model;
+
+/* Represent an employee in the leave management system  */
+public class Employee {
+
+    private ind id;
+    private String name;
+    private String email;
+    private String role;
+
+    // Constructor
+    public Employee(int id, String name, String email, String role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
